75857f86d690371732bfc09c34f2ce1f4561c68f
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 00:58:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,21 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max = array[i]
+  array.size.times do |i|
+    if(array[i]>max)
+      max = array[i]
+    end
+  end
+  return max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  max = array[i]
+  array.size.times do |i|
+    if(array[i]<max)
+      max = array[i]
+    end
+  end
+  return max  # Add your solution here
 end
